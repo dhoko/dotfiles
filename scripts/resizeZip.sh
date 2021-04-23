952fe19bd6c4f81268d09daf9d5ab9ed59ca2d0f
dhoko <dhoko@pm.me>
2021-04-23 14:47:42 +0200

    Upgrade resizeZip

--- scripts/resizeZip.sh
+++ scripts/resizeZip.sh
@@ -1,38 +1,68 @@
 #!/usr/bin/env bash
 set -eo pipefail
+
 IFS=$'\n'
+IS_DEBUG=false;
+
+
+
 function getExtension {
-  local file =$(basename -- "$1");
-  echo "${1##*.}";
+  local file=$(basename -- "$1");
+  local ext="${1##*.}";
+  if [[ "$ext" == "png" ]]; then
+       echo "$ext";
+  fi;
+  echo "JPEG";
 }
 
 function convertImages {
+
   cd "$1";
-  for file in $(ls *.{jpg,jpeg,png})
-  do
+  ls -lh;
+  
+  for file in $(find * -type f -name "*.jpg" -or -name "*.jpeg" -or -name ".png"); do
+  #for file in **/*.{jpg,jpeg,png}; do
+  #for file in $(ls *.{jpg,jpeg,png});  do
+
+    local ext=$(getExtension "$file" | tr '[:lower:]' '[:upper:]');
     local size=$(identify "$file" | awk '{print $3}' | awk -F x '{print $2}');
-    if [ "$size" -ge 2400 ]; then
+
+
+    if [ "$IS_DEBUG" = true ]; then
+        echo "[File] $file";
+        echo "[Size] $size";
+        echo "[Ext] $ext";
+    fi;
+
+    # if [ "$size" -ge 2400 ]; then
+    #   echo "[process]: $file";
+    #   convert "$file" -resize 50% "$file";
+    # fi
+    if [ "$size" -ge 1400 ]; then
       echo "[process]: $file";
-      convert "$file" -resize 50% "$file";
+      convert -resize x1400 "$file" "$file"
+
+      if [ "$ext" = 'PNG' ]; then
+        pngquant --quality=70-80 "$file"
+      fi
     fi
 
-    if [ "$size" -ge 2000 ] && [ "$size" -lt 2400 ]; then
-      echo "[process]: $file";
-      convert "$file" -resize 70% "$file";
-    fi
+    # if [ "$size" -ge 2000 ] && [ "$size" -lt 2400 ]; then
+    #   echo "[process]: $file";
+    #   convert "$file" -resize 70% "$file";
+    # fi
   done;
   cd -
 }
 
 function main {
-  for zip in $(ls *.{zip,rar})
-  do
+  for zip in $(ls *.{zip,rar}); do
     local name=$(basename -- "$zip");
     local ext="${name##*.}";
     local dir="${name%.*}";
 
     if [[ "$ext" = "zip" ]]; then
-      unzip "$zip" -d "$dir";
+      unzip -qq "$zip" -d "$dir";
     fi;
 
     if [[ "$ext" = "rar" ]]; then
@@ -40,10 +70,21 @@ function main {
     fi;
 
     convertImages "$dir";
-    zip -r "$dir-2.zip" "$dir"
+    zip -qq -r "$dir-2.zip" "$dir"
     rm -r "$dir";
   done;
 }
+
+if [[ "$1" == "resize" ]]; then
+    convertImages "$2" "--debug"
+    exit;
+fi;
+
+
+if [[ "$*" =~ "--debug" ]]; then
+  IS_DEBUG=true;
+fi;
+
 
 main;
 IFS='
